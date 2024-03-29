6268f5edfdb6cf9ff2ef6376ac34401edf350ab0
debasisray16 <debasishray3275@gmail.com>
2024-03-29 23:51:16 +0530

    New ML Model

---
+++ Debasish Test/V2/main.py
@@ -0,0 +1,34 @@
+import streamlit as st
+from datetime import datetime
+import yfinance as yf
+from fbprophet import Prophet
+from fbprophet.plot import plot_plotly
+from plotly import graph_objs as go
+
+
+START = "2015-01-01"
+TODAY = date.today().strftime("%Y-%m-%d")
+st.title("Stock Prediction App")
+
+
+stocks=("AAPL","GOOG","MSFT","GME")
+
+selected_stock=st.selectbox("Select dataset for prediction",stocks)
+
+n_years=st.slider("Years of Prediction: ",1,4)
+period = n_years *365
+
+
+@st.cache
+def load_data(ticker):
+    data = yf.download(ticker,START,TODAY)
+    data.reset_index(inplace=True)
+    return data
+
+
+data_load_state=  st.text("Load data .. ")
+data=load_data(selected_stock)
+data_load_state.text("Loading data... done!")
+
+st.subheader('Raw data')
+st.write(data.tail())
