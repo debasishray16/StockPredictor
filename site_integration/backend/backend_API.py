cd217727d3fb59e714f8c43cc8f69bc2f47aea9a
Debasish Ray <debasishray3275@gmail.com>
2024-10-22 22:39:41 +0530

    file name changed

--- site_integration/backend/backend_API.py
+++ site_integration/backend/backend_API.py
@@ -39,7 +39,7 @@ def predict():
     data_training_array = scaler.fit_transform(data_training)
 
     # Load Model
-    model = tf.keras.models.load_model('../Model-Modified/8_15_23_125_LXg.h5', compile=False)
+    model = tf.keras.models.load_model('../model/8_15_23_125_LXg.h5', compile=False)
 
     # Testing part
     past_100_days = data_training.tail(100)
