333a734e3a0ee5a3067275392d43d3cebf036157
Utkarsh Raj Sinha <ur5441@srmist.edu.in>
2024-10-23 00:06:39 +0530

    Model update on site

--- site_integration/src/Components/companyDesc.js
+++ site_integration/src/Components/companyDesc.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { IoChevronDown, IoChevronUp } from 'react-icons/io5';
 
-const CompanyDesc = ({ companyDescription }) => {
+const CompanyDesc = ({ companyDescription, loading }) => {
   const [isCollapsed, setIsCollapsed] = useState(true);
 
   // Function to toggle collapse state
@@ -30,9 +30,12 @@ const CompanyDesc = ({ companyDescription }) => {
 
       {/* Collapsible content with animation */}
       <div className={`transition-all duration-500 ease-in-out overflow-hidden ${isCollapsed ? 'max-h-0' : 'max-h-screen'}`}>
-        <p className='text-[#e6e7ec] px-7 p-4' style={{ textAlign: 'justify' }}>
-          {companyDescription}
-        </p>
+        {loading ? (
+          <p className='text-[#e6e7ec] px-7 p-4' style={{ textAlign: 'justify' }}>
+            {companyDescription}
+          </p>
+        ) : (<p className="text-[#e6e7ec] text-[15px] leading-[34px] font-normal px-7 p-4 flex items-center justify-center">No ticker selected</p>
+        )}
       </div>
     </div>
   );
